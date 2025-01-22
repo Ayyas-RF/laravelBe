6d553433df4eefebb37f1d5be2ccd7208c1176d7
Salskuye <safarisyi564@gmail.com>
2025-01-22 12:11:22 +0700

    Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -13,10 +14,13 @@ class ProductController extends Controller
     public function index()
     {
         $data = Product::all();
-        return response ([
-            "message" => "Product has been created",
-            "data" => $data
-        ]);
+        return $data->count() > 0 ? response([
+            "message" => "Product has been founded",
+            'data' => $data
+        ], 200) : response([
+            "message" => "Product has been founded",
+            'data' => $data
+        ], 404);
     }
 
     /**
@@ -24,40 +28,104 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate ([
-            'product_name' => 'required|string|unique:products,product_name'
+        $request->validate([
+            "category_id" => "required|exists:categories,id",
+            "product_name" => "required|unique:products,product_name",
+            "product_image" => "required|image:jpeg,jpg,png",
+            "price" => "required|integer",
+            "stock" => "required|integer",
+            "description" => "required|string",
         ]);
+        $image_name = time() . "." . $request->product_image->extension();
+        $request->product_image->move(public_path("upload/product"),
+        $image_name);
 
-        Product::create ([
-            'products_name' => $request->category_name
+        Product::create([
+            "category_id" => $request->category_id,
+            "product_name" => $request->product_name,
+            "product_image" => url("/upload/product") . "/" . $image_name,
+            "product_image_name" => $image_name,
+            "price" => $request->price,
+            "stock" => $request->stock,
+            "description" => $request->description,
         ]);
-
-        return response ([
-            "message" => "Product has been created",
-        ]);
+        return response([
+            "message" => "Product has been created succesfully!"
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(product $product)
+    public function show(Product $product)
     {
-        //
+        $data = Product::find($product);
+        return isset($data) ?
+        response([
+            "message" => "Product detail has been founded",
+            'data' => $data
+        ], 200) : response ([
+            "message" => "Product has founded",
+            'data' => $data
+        ], 404);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, product $product)
+    public function update(Request $request, string
+     $product)
     {
-        //
+        $request->validate([
+            "category_id" => "required|exists:categories,id",
+            "product_name" => "required|unique:products,product_name",
+            "price" => "required|integer",
+            "stock" => "required|integer",
+            "description" => "required|string",
+        ]);
+        $data = Product::find($product);
+        if (!isset($data)) {
+            return response([
+                "message" => "Product has not found!"
+            ], 404);
+        }
+        if (isset($request->product_image)){
+            $request->validate([
+                "product_image" => "required|image:jpeg,jpg,png"
+            ]);
+
+            $request->product_image->move(public_path("upload/product"), $data->product_image_name);
+        }
+
+        $data->category_id = $request->category_id;
+        $data->product_name = $request->product_name;
+        $data->price = $request->price;
+        $data->stock = $request->stock;
+        $data->description = $request->description;
+        $data->save();
+
+        return response ([
+            "message" => "Product has been updated!"
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(product $product)
+    public function destroy(Product $product)
     {
-        //
+        $data = Product::find($product);
+        if (!isset($data)) {
+            return response([
+                "message" => "Product has not found!"
+            ], 404);
+        }
+        File::delete(public_path("upload/product") . "/" . $data->product_image_name);
+
+        $data->delete();
+
+        return response([
+            "message" => "Product has been deleted!"
+        ], 200);
     }
 }
